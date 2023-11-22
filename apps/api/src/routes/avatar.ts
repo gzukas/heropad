57d06e4148e7e54df2a38072b2ab2af94d59152f
gzukas <gedxaz@gmail.com>
2023-11-22 12:27:29 +0200

    change typings of avatar route params

--- apps/api/src/routes/avatar.ts
+++ apps/api/src/routes/avatar.ts
@@ -2,21 +2,22 @@ import { createAvatar } from '@dicebear/core';
 import { avataaars } from '@dicebear/collection';
 import { FastifyPluginAsync } from 'fastify';
 
-interface AvatarRequest {
-  Params: {
-    hero: string;
-  };
+interface AvatarParams {
+  hero: string;
 }
 
 export const avatarRoutes: FastifyPluginAsync = async server => {
-  server.get<AvatarRequest>('/avatars/:hero.svg', (request, reply) => {
-    const { hero } = request.params;
+  server.get<{ Params: AvatarParams }>(
+    '/avatars/:hero.svg',
+    (request, reply) => {
+      const { hero } = request.params;
 
-    reply.header('Content-Type', 'image/svg+xml');
-    reply.header('Content-Disposition', `inline; filename="${hero}.svg"`);
-    reply.header('Cache-Control', `max-age=${365 * 24 * 60 * 60}`);
-    reply.header('X-Robots-Tag', 'noindex');
+      reply.header('Content-Type', 'image/svg+xml');
+      reply.header('Content-Disposition', `inline; filename="${hero}.svg"`);
+      reply.header('Cache-Control', `max-age=${365 * 24 * 60 * 60}`);
+      reply.header('X-Robots-Tag', 'noindex');
 
-    return createAvatar(avataaars, { seed: hero, size: 48 }).toString();
-  });
+      return createAvatar(avataaars, { seed: hero, size: 48 }).toString();
+    }
+  );
 };
