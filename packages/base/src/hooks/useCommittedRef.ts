7aa85dd8ff97a96f5eeca39c7a4916e34200ed1a
gzukas <gedxaz@gmail.com>
2023-11-28 17:06:36 +0200

    fix type in hook name

--- packages/base/src/hooks/useCommittedRef.ts
+++ packages/base/src/hooks/useCommittedRef.ts
@@ -1,6 +1,6 @@
 import { useRef, useEffect } from 'react';
 
-export function useCommitedRef<T>(value: T) {
+export function useCommittedRef<T>(value: T) {
   const ref = useRef(value);
   useEffect(() => {
     ref.current = value;
