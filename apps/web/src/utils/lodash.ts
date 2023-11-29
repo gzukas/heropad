1c8e20d82b6db443406b8fea19a2b7245dd210d1
gzukas <gedxaz@gmail.com>
2023-11-30 00:45:38 +0200

    add missing deps

--- apps/web/src/utils/lodash.ts
+++ apps/web/src/utils/lodash.ts
@@ -1,5 +1,5 @@
 export * from 'lodash-es';
 export { isEqual as _isEqual } from 'lodash-es';
 
-// Reduce unzipped bundle size (-12kB) by using more compact `isEqual` needed for react-sigma.
+// Reduce bundle size (-12kB, uncompressed) by using more compact `isEqual` needed for react-sigma.
 export { default as isEqual } from 'fast-deep-equal';
