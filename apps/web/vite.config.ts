b1f277d1ca1dad808a4899635f252727c2f33301
gzukas <gedxaz@gmail.com>
2023-12-12 12:44:57 +0200

    build: add vendor-graph chunk

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { lingui } from '@lingui/vite-plugin';
 
+function createManualChunks(chunks: Record<string, RegExp>) {
+  const chunkEntries = Object.entries(chunks);
+  return (id: string) => chunkEntries.find(([, re]) => re.test(id))?.[0];
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -17,6 +22,16 @@ export default defineConfig({
     lingui(),
     splitVendorChunkPlugin()
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: createManualChunks({
+          'vendor-graph':
+            /\/sigma|@yomguithereal\/helpers|events|graphology|mnemonist|obliterator|pandemonium/
+        })
+      }
+    }
+  },
   resolve: {
     alias: {
       lodash: 'src/utils/lodash.ts'
