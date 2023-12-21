41e4dfe24e45b4b4b74c809caae9b5399026a07b
gzukas <gedxaz@gmail.com>
2023-12-21 13:38:08 +0200

    fix: do not drop search params on award unpin

--- apps/web/src/routes/$hero/$awardId/index.tsx
+++ apps/web/src/routes/$hero/$awardId/index.tsx
@@ -30,6 +30,7 @@ export const awardRoute = new Route({
           <IconButtonLink
             to="/$hero"
             params={{ hero }}
+            search={prev => prev}
             edge="end"
             sx={{ ml: 0.5 }}
           >
