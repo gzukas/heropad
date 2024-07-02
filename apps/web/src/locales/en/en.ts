5fee1f59d66e439cbd633821e3808b1b57b42af4
gzukas <gedxaz@gmail.com>
2024-07-02 18:56:22 +0300

    refactor: use Map.groupBy instead

--- apps/web/src/locales/en/en.ts
+++ apps/web/src/locales/en/en.ts
@@ -1,3 +1,3 @@
-/*eslint-disable*/ export const messages = JSON.parse(
+export const messages = JSON.parse(
   '{"oosprE":"Awards","+DEogc":"Change language","yz7wBu":"Close","nBSpV8":"Focus on me","6DGeau":"Given","kKi2Hf":"Heroes","EpWWC+":["No results have been found for \\"<0>",["debouncedSearchTerm"],"</0>\\"."],"fZ5Vnu":"Received","jKibyt":"Reset zoom","A1taO8":"Search","v78bJh":"Search for heroes and awards...","ozu0F0":"Turn off the light","nD+TME":"Turn on the light","gcARWC":"Unpin award","AWOSPo":"Zoom in","FjkaiT":"Zoom out"}'
 );
