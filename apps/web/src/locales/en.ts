b64b85b8d9bb7a3e44a1223fb10590fd86c8b65f
gzukas <gedxaz@gmail.com>
2024-09-18 02:15:47 +0300

    fix: recompile messages

---
+++ apps/web/src/locales/en.ts
@@ -0,0 +1,4 @@
+  import type { Messages } from '@lingui/core';
+export const messages = JSON.parse(
+  '{"oosprE":"Awards","+DEogc":"Change language","yz7wBu":"Close","nBSpV8":"Focus on me","6DGeau":"Given","kKi2Hf":"Heroes","AxPAXW":"No results found","fZ5Vnu":"Received","jKibyt":"Reset zoom","A1taO8":"Search","v78bJh":"Search for heroes and awards...","ozu0F0":"Turn off the light","nD+TME":"Turn on the light","gcARWC":"Unpin award","AWOSPo":"Zoom in","FjkaiT":"Zoom out"}'
+) as Messages;
